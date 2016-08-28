2bdafcc9a2b22a735b0888abf04808ad7ee17f48
STZHANGJK <stzhangjk@163.com>
2016-08-28 14:14:24 +0800

    006-sql代码段

---
+++ 006-Tag_Sql/src/test/java/TestConfiguration006.java
@@ -0,0 +1,35 @@
+
+
+import entity.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by STZHANGJK on 2016/8/27.
+ */
+public class TestConfiguration006 {
+
+    private static SqlSessionFactory sf;
+
+    @BeforeClass
+    public static void config() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        sf = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    @Test
+    public void test() {
+        try(SqlSession session = sf.openSession()){
+            User user = session.selectOne("UserMapper.select",1);
+            System.out.println(user.getUsername());
+        }
+    }
+}
